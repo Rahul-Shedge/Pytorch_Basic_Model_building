d6d3900ce30b0aa48885809063ad32b6f959853e
Rahul-Shedge <rahulshedge555@outlook.com>
2023-07-30 17:22:26 +0530

    data_preprocessing updated

--- src/stage_01_data_preprocessing.py
+++ src/stage_01_data_preprocessing.py
@@ -1,7 +1,38 @@
+import requests
+import zipfile
 import torch
+import yaml
 from torch import nn
+from pathlib import Path
 from trochvision import datasets, transform
 from torch.utils.data import Dataloader
+from src.utils import read_yaml
+
+
+def data_extraction():
+    configs = read_yaml("./configs.yaml")
+    # Setup path to data folder
+    data_path = Path(configs["data"]["data_folder"])
+    image_path = data_path / configs["data"]["image_folder"]
+
+    # If the image folder doesn't exist, download it and prepare it... 
+    if image_path.is_dir():
+        print(f"{image_path} directory exists.")
+    else:
+        print(f"Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+        
+        # Download pizza, steak, sushi data
+        with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+            request = requests.get(configs["data"]["data_source"])
+            print("Downloading pizza, steak, sushi data...")
+            f.write(request.content)
+
+        # Unzip pizza, steak, sushi data
+        with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+            print("Unzipping pizza, steak, sushi data...") 
+            zip_ref.extractall(image_path)
+
 
 
 NUM_WORKERS = os.cpu_count()
@@ -49,7 +80,7 @@ def create_dataloaders(
         transform= test_transform
     )
 
-      # Get class names
+    # Get class names
     class_names = train_data.classes
 
 
@@ -73,3 +104,13 @@ def create_dataloaders(
     )
 
     return TrainDataloader,TestDataloader, class_names
+
+
+
+
+
+
+
+
+
+
